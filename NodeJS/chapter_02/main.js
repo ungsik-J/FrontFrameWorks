a1f52439ee9e9b38a42f9132069e2472c9bea114
ungsik-J <40648589+ungsik-J@users.noreply.github.com>
2025-06-04 00:00:39 +0900

    콘솔에서의 입력값

--- NodeJS/chapter_02/main.js
+++ NodeJS/chapter_02/main.js
@@ -1,3 +1,17 @@
-var args = process.argv;
+/**
+ * 콘솔에서의 입력값
+ * https://opentutorials.org/module/3549/21062
+ * 
+ * node main.js args
+ *  [
+    'C:\\Program Files\\nodejs\\node.exe',
+    'C:\\devHome\\★gItRepository\\FrontFrameWorks\\NodeJS\\chapter_02\\main.js',
+    'args'
+    ]
+ */
+const { argv } = require('node:process');
 
-console.log( args )
+// print process.argv
+argv.forEach((val, index) => {
+  console.log(`${index}: ${val}`);
+});
